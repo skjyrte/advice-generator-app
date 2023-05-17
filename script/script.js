216d70d7d8d9ce945855d793f2b8ca5c6e58cf6d
skjyrte <103749024+skjyrte@users.noreply.github.com>
2023-05-17 22:47:29 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,18 @@
+function getAdvice() {
+  fetch("https://api.adviceslip.com/advice", { cache: "no-cache" })
+    .then((response) => response.json())
+    .then((obj) => obj.slip)
+    .then((obj) => obj)
+    .then((obj) => {
+      document.querySelector(".adviceContent").classList.remove("errorClass");
+      document.querySelector(".adviceNo").innerHTML = obj.id;
+      document.querySelector(".adviceContent").innerHTML = obj.advice;
+    })
+    .catch(() => {
+      document.querySelector(".adviceContent").innerHTML =
+        "Page has encountered an error. Please try again later.";
+      document.querySelector(".adviceContent").classList.add("errorClass");
+    });
+}
+
+document.querySelector(".diceImage>img").addEventListener("click", getAdvice);
