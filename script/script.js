350aeebb317fa6126ce1796d6b356056160c2c45
skjyrte <103749024+skjyrte@users.noreply.github.com>
2023-05-20 18:38:54 +0200

    minor updates

--- script/script.js
+++ script/script.js
@@ -1,22 +1,23 @@
+const adviceContent = document.querySelector(".adviceContent");
+const adviceNumber = document.querySelector(".adviceNo");
+
 function getAdvice() {
   fetch("https://api.adviceslip.com/advice", { cache: "no-cache" })
     .then((response) => response.json())
-    .then((obj) => obj.slip)
-    .then((obj) => obj)
-    .then((obj) => {
-      document.querySelector(".adviceContent").classList.remove("errorClass");
-      document.querySelector(".adviceNo").innerHTML = `ADVICE #${obj.id}`;
-      document.querySelector(".adviceContent").innerHTML = `"${obj.advice}"`;
+    .then(({ slip: { id, advice } }) => {
+      adviceContent.classList.remove("errorClass");
+      adviceNumber.innerHTML = `ADVICE #${id}`;
+      adviceContent.innerHTML = `"${advice}"`;
       document.documentElement.style.setProperty("--fontSize", "28px");
-      if (obj.advice.length > 80) {
+      if (advice.length > 80) {
         document.documentElement.style.setProperty("--fontSize", "22px");
       }
     })
     .catch(() => {
-      document.querySelector(".adviceContent").innerHTML =
+      adviceContent.innerHTML =
         "Page has encountered an error. Please try again later.";
-      document.querySelector(".adviceNo").innerHTML = "";
-      document.querySelector(".adviceContent").classList.add("errorClass");
+      adviceNumber.innerHTML = "";
+      adviceContent.classList.add("errorClass");
     });
 }
 
